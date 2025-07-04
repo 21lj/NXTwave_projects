04f27b6299520b3fd96c30fe5d05630eb8983a31
Lijo Joseph <lijojoseph0021@gmail.com>
2025-07-04 14:38:55 +0530

    fix CORS for vercel

--- GrowAiProject/backend/index.js
+++ GrowAiProject/backend/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const cors = require('cors')
 
 const app=express()
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: '*', 
+}));
+
 app.use(express.json())
 
 const PORT = 3001
