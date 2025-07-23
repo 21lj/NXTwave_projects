dbd9e97d515f87aef76cb20f0fc8dc096d57025b
Lijo Joseph <lijojoseph0021@gmail.com>
2025-07-23 18:11:24 +0530

    minor update

--- ResumeAnalyzer/backend/server.js
+++ ResumeAnalyzer/backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use('/api/resumes', resumeRoutes);
 
