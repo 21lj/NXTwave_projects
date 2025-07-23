115a7e4cb3eb8db86d21edcfc801311c19ff1161
Lijo Joseph <lijojoseph0021@gmail.com>
2025-07-23 18:22:28 +0530

    minor update

--- ResumeAnalyzer/backend/server.js
+++ ResumeAnalyzer/backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+const buildPath = path.join(__dirname, '../frontend/build')
+console.log("Configured build path for static files:", buildPath)
+app.use(express.static(buildPath))
 
 app.use('/api/resumes', resumeRoutes);
 
@@ -18,6 +20,6 @@ app.get('/', (req, res) => {
     res.send('Resume Analyzer Backend Server is Running!');
 });
 
-app.listen(PORT, () => {
-    console.log(`\n Resume Analyzer Backend Server is running on http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0',() => {
+    console.log(`\n Resume Analyzer Backend Server is running on http://0.0.0.0:${PORT}`)
 });
